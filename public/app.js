30ce01d993b6c79c82a87a75a60a3d3c41b2b8e5
miriuchijima <ads10331@gmail.com>
2017-06-25 22:24:06 +0900

    ok

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@
 var learnjs = {};
 
 learnjs.problemView = function(problemNumber){
-    var title = 'Problem #' + problemNumber + 'Coming soon!';
+    var title = 'Problem #' + problemNumber + ' Coming soon!';
     return $('<div class="problem-view">').text(title);
 }
 
