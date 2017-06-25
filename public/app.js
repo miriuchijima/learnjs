7374bba9df79a486fdd23f10674d658dc114fbd4
miriuchijima <ads10331@gmail.com>
2017-06-25 11:14:34 +0900

    ok

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 'use strict'
 var learnjs = {};
 
-learns.showView = function(hash){
+learnjs.showView = function(hash){
     var problemView = $('<div class="problem-view>').text('Coming soon!');
     $('.view-container').empty().append(problemView);
 }
