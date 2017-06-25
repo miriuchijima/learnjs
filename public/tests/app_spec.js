f0b460080c6cdf13889ee28ed0c6e82f9263cb66
miriuchijima <ads10331@gmail.com>
2017-06-25 12:16:24 +0900

    ok

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -2,7 +2,10 @@ describe('LearnJS',function(){
     it('can show a problem view',function(){
        learnjs.showView('#problem-1');
        expect($('.view-container .problem-view').length).toEqual(1);
-       });    
+       });
+    it('shows the landing page view when there is no hash',function(){
+       expect($('.view-container .landing-view').length).toEqual(1);
+    });
 });
 
 
